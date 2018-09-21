ea758f6f6988ed20f8ec5ba83e63412592251149
Suttipong Montreewong <suttipong.mont@gmail.com>
2018-09-22 06:47:43 +0700

    back to antd

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,27 +1,21 @@
 import React from "react";
-// import { Layout } from "antd";
-import { NavBar, WhiteSpace, WingBlank } from "antd-mobile";
+import { Layout } from 'antd';
 
-// const { Footer, Content } = Layout;
+const { Header, Content, Footer } = Layout;
+
 
 const MainLayout = ({ children }) => (
-  <div>
-    <NavBar mode="dark">ICMM 2019</NavBar>
-    <WingBlank size="lg">
-      <WhiteSpace size="lg" />
-      <div>{children}</div>
-      <WhiteSpace size="lg" />
-    </WingBlank>
-  </div>
-  //     <Content style={{ padding: "0 50px", marginTop: 96 }}>
-  //       <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
-  //         {children}
-  //       </div>
-  //     </Content>
-  //     <Footer style={{ textAlign: "center" }}>
-  //       © 2018 grouppe.co, All Rights Reserved.
-  //     </Footer>
-  //   </Layout>
+  <Layout style={{ minHeight: '100vh' }}>
+  <Header style={{ backgroundColor: '#c43a43', textAlign: 'center'}}>
+    <h1 style={{ color: 'white'  }}>ICMM 2019</h1>
+  </Header>
+  <Content style={{ padding: '10px 10px 0 10px' }}>
+    <div style={{ background: '#fff', padding: 16, minHeight: 280 }}>{ children }</div>
+  </Content>
+  <Footer style={{ textAlign: 'center' }}>
+    © ICMM 2019 Powered by Grouppe.co
+  </Footer>
+</Layout>
 );
 
 export default MainLayout;
--- src/containers/RegistrationForm.js
+++ src/containers/RegistrationForm.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import { ReCaptcha } from "react-recaptcha-google";
-import { List, InputItem, DatePicker, Button, Card } from "antd-mobile";
-import { createForm } from "rc-form";
+import { Form, Input, DatePicker, Button, Select} from 'antd';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
 
 class RegistrationForm extends Component {
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: [],
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  handleConfirmBlur = (e) => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  }
   constructor(props, context) {
     super(props, context);
     this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
@@ -25,66 +43,330 @@ class RegistrationForm extends Component {
     this.setState("recaptchaResponse", recaptchaToken);
   }
 
-  render() {
-    const { getFieldProps } = this.props.form;
+  renderPage = () => {
+    const { getFieldDecorator } = this.props.form;
 
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+    return [(<Form onSubmit={this.handleSubmit}>
+      <FormItem
+        {...formItemLayout}
+        label="Surname"
+      >
+        {getFieldDecorator('surname', {
+          rules: [ {
+            required: true, message: 'Please input your surname!',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Tel"
+      >
+        {getFieldDecorator('tel', {
+          rules: [{
+            required: true, message: 'Please input your telephone no.!',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Birth date"
+      >
+        {getFieldDecorator('birthdate', { rules: [{ type: 'object', required: true, message: 'Please select birth date!' }] })(
+          <DatePicker />
+        )}
+      </FormItem>
+      <FormItem
+        {...tailFormItemLayout}
+      >
+      <div style={{ overflow: 'hidden'}}>
+        <ReCaptcha
+              ref={el => {
+                this.captchaDemo = el;
+              }}
+              size="normal"
+              render="explicit"
+              sitekey="6LfP_m8UAAAAAGnl6CDkJ1i5IQ3HjpOCcahw6KQW"
+              onloadCallback={this.onLoadRecaptcha}
+              verifyCallback={this.verifyCallback}
+            />
+      </div>
+      </FormItem>
+      <FormItem {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">Go!!</Button>
+      </FormItem>
+    </Form>),(<Form onSubmit={this.handleSubmit}>
+      <FormItem
+        {...formItemLayout}
+        label="Surname"
+      >
+        {getFieldDecorator('surname', {
+          rules: [ {
+            required: true, message: 'Please input your surname!',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Tel"
+      >
+        {getFieldDecorator('tel', {
+          rules: [{
+            required: true, message: 'Please input your telephone no.!',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Birth date"
+      >
+        {getFieldDecorator('birthdate', { rules: [{ type: 'object', required: true, message: 'Please select birth date!' }] })(
+          <DatePicker />
+        )}
+      </FormItem>
+      <FormItem
+        {...tailFormItemLayout}
+      >
+      <div style={{ overflow: 'hidden'}}>
+        <ReCaptcha
+              ref={el => {
+                this.captchaDemo = el;
+              }}
+              size="normal"
+              render="explicit"
+              sitekey="6LfP_m8UAAAAAGnl6CDkJ1i5IQ3HjpOCcahw6KQW"
+              onloadCallback={this.onLoadRecaptcha}
+              verifyCallback={this.verifyCallback}
+            />
+      </div>
+      </FormItem>
+      <FormItem {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">Go!!</Button>
+      </FormItem>
+    </Form>)
+    ]
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
     return (
-      <Card>
-        <Card.Body>
-          <List>
-            <InputItem
-              {...getFieldProps("surname")}
-              type="text"
-              placeholder="Dalton"
-            >
-              Surname
-            </InputItem>
-            <InputItem
-              {...getFieldProps("tel")}
-              type="phone"
-              placeholder="123 456 7890"
-            >
-              Tel
-            </InputItem>
-            <DatePicker
-              {...getFieldProps("birthdate", {
-                rules: [
-                  { required: true, message: "Must select a date" },
-                  { validator: this.validateDatePicker }
-                ]
-              })}
-              title="Select Birth Date"
-              mode="date"
-            >
-              <List.Item arrow="horizontal">Birth Date</List.Item>
-            </DatePicker>
-            <List.Item>
-              <ReCaptcha
-                ref={el => {
-                  this.captchaDemo = el;
-                }}
-                size="normal"
-                render="explicit"
-                sitekey="6LfP_m8UAAAAAGnl6CDkJ1i5IQ3HjpOCcahw6KQW"
-                onloadCallback={this.onLoadRecaptcha}
-                verifyCallback={this.verifyCallback}
-              />
-            </List.Item>
-            <List.Item>
-              <Button
-                type="primary"
-                size="small"
-                inline
-                onClick={this.onSubmit}
-              >
-                Go!!
-              </Button>
-            </List.Item>
-          </List>
-        </Card.Body>
-      </Card>
+      <div>
+      {this.renderPage()[0]}
+      <Form onSubmit={this.handleSubmit}>
+      <FormItem
+        {...formItemLayout}
+        label="ชื่อ"
+      >
+        {getFieldDecorator('firstname', {
+          rules: [ {
+            required: true, message: 'กรุณากรอกชื่อ',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="นามสกุล"
+      >
+        {getFieldDecorator('surname', {
+          rules: [ {
+            required: true, message: 'กรุณากรอกนามสกุล',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="วันเกิด"
+      >
+        {getFieldDecorator('birthdate', { rules: [{ type: 'object', required: true, message: 'กรุณาเลือกวันเกิด' }] })(
+          <DatePicker />
+        )}
+      </FormItem>
+      <FormItem
+          {...formItemLayout}
+          label="เพศ"
+          hasFeedback
+        >
+          {getFieldDecorator('gender', {
+            rules: [
+              { required: true, message: 'กรุณาเลือกเพศ' },
+            ],
+          })(
+            <Select placeholder="เลือกเพศ">
+              <Option value="male">ชาย</Option>
+              <Option value="female">หญิง</Option>
+            </Select>
+          )}
+        </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Tel."
+      >
+        {getFieldDecorator('tel', {
+          rules: [{
+            required: true, message: 'กรุณากรอกหมายเลขโทรศัพท์',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+          {...formItemLayout}
+          label="E-mail"
+        >
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'อีเมลไม่ถูกต้อง',
+            }, {
+              required: true, message: 'กรุณากรอกอีเมล',
+            }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+        {...formItemLayout}
+        label="Emergency Contact"
+      >
+        {getFieldDecorator('emergency_contact', {
+          rules: [{
+            required: true, message: 'กรุณากรอกผู้ติดต่อสำหรับกรณีฉุกเฉิน',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+        {...formItemLayout}
+        label="Emergency Tel."
+      >
+        {getFieldDecorator('emergency_tel', {
+          rules: [{
+            required: true, message: 'กรุณากรอกหมายเลขโทรศัพท์ฉุกเฉิน',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem
+          {...formItemLayout}
+          label="Shirt size"
+          hasFeedback
+        >
+          {getFieldDecorator('shirt_size', {
+            rules: [
+              { required: true, message: 'กรุณาเลือกเพศ' },
+            ],
+          })(
+            <Select placeholder="เลือกขนาดเสื้อ">
+              <Option value="xs">XS - 34"</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Running type"
+          hasFeedback
+        >
+          {getFieldDecorator('running_type', {
+            rules: [
+              { required: true, message: 'กรุณาประเภทการวิ่ง' },
+            ],
+          })(
+            <Select placeholder="เลือกประเภทการวิ่ง">
+              <Option value="3">Fun run - 3 km"</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Alumni"
+          hasFeedback
+        >
+          {getFieldDecorator('alumni', {
+            rules: [
+              { required: true, message: 'กรุณาเลือก Alumni' },
+            ],
+          })(
+            <Select placeholder="เลือก Alumni">
+              <Option value="alumni">ศิษย์เก่า</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+        {...formItemLayout}
+        label="ชื่อบนบิล"
+      >
+        {getFieldDecorator('name_on_bill', {
+          rules: [{
+            required: true, message: 'กรุณากรอกชื่อบนบิล',
+          }],
+        })(
+          <Input />
+        )}
+      </FormItem>
+      <FormItem {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">Go!!</Button>
+      </FormItem>
+    </Form>
+      </div>
     );
   }
 }
 
-export default createForm()(RegistrationForm);
+export default Form.create()(RegistrationForm);
