bcb5f943bb65d9291f1c78157d0081eb2c70f8b4
Pathompong Chaisri <kiraboshi47@hotmail.com>
2018-10-09 10:43:13 +0700

    เพิ่มเตือนว่าไม่คืนเงิน ถ้าไม่ใช่จุฬา ในหน้า /series

--- src/containers/SeriesAuth.js
+++ src/containers/SeriesAuth.js
@@ -41,6 +41,7 @@ class SeriesAuth extends Component {
   render() {
     return (
       <div style={{ display: "flex", flexFlow: "column wrap" }}>
+     
         <Input
           onChange={this.handleChange.bind(this)}
           placeholder="Please enter invitation code"
@@ -48,7 +49,7 @@ class SeriesAuth extends Component {
             width: "80vw",
             maxWidth: "300px",
             margin: "0 auto",
-            marginTop: "15vh"
+            marginTop: "10vh"
           }}
         />
         <Button
@@ -64,6 +65,10 @@ class SeriesAuth extends Component {
           {" "}
           ยืนยัน{" "}
         </Button>
+        <p  style={{    fontSize: "11px", paddingTop: "7vh"}} >
+      *  การสมัครของแต่ละรุ่นจะถูกกำหนดโดยโค้วต้าของรุ่น Code ที่ได้รับไปนั้น จะใช้ได้ <span style={{color:"red"}}>1 Code ต่อ 1 ผู้สมัคร</span> จากนั้น Code จะถูก ตัดออกจากจำนวนโค้วต้าของแต่ละรุ่น 
+      และหากการสมัครถูกใช้โดยบุคคลภายนอกที่ไม่ได้เป็นส่วนหนึ่งของศิษย์ก่า วิศวฯ จุฬาฯ และครอบครัว จะมีการตัดสิทธิการสมัคร <span style={{color:"red"}}>โดยไม่มีนโยบายการคืนเงินค่าสมัครใดๆ </span>
+      </p>
       </div>
     );
   }
